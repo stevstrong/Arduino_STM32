fd4f7d8f18ed0d28b908e76a09bf6b0230681594
stevstrong <stevestrong@online.de>
2018-12-27 09:51:54 +0100

    F4: Ethernet: fix GPIO settings

--- STM32F4/cores/maple/libmaple/ethernet.c
+++ STM32F4/cores/maple/libmaple/ethernet.c
@@ -15,17 +15,13 @@ ETH_REF_CLK, ETH_MDIO, ETH_CRS_DV, ETH_TX_EN, ETH_TX_D0, ETH_TX_D1, ETH_MDC, ETH
 };
 
 
-//-----------------------------------------------------------------------------
-void ethernet_init(void)
-{
-  /* Select MII or RMII Mode*/
+// Select MII or RMII Mode/
 #define ETH_MEDIA_INTERFACE_MII       0x00000000U
 #define ETH_MEDIA_INTERFACE_RMII      ((uint32_t)SYSCFG_PMC_MII_RMII_SEL)
 
-  SYSCFG->PMC &= ~(SYSCFG_PMC_MII_RMII_SEL_Msk);
-  // enable the following line for RMII
-  SYSCFG->PMC |= ETH_MEDIA_INTERFACE_RMII;
-
+//-----------------------------------------------------------------------------
+void ethernet_init(void)
+{
 	rcc_clk_enable(RCC_ETHMAC);
 	rcc_clk_enable(RCC_ETHMACTX);
 	rcc_clk_enable(RCC_ETHMACRX);
@@ -46,11 +42,15 @@ void ethernet_deinit(void)
 //-----------------------------------------------------------------------------
 void ethernet_gpio_init(void)
 {
+  SYSCFG->PMC &= ~(SYSCFG_PMC_MII_RMII_SEL_Msk);
+  // enable the following line for RMII
+  SYSCFG->PMC |= ETH_MEDIA_INTERFACE_RMII;
+
 	// setup pins
 	for (uint8_t i = 0; i<sizeof(eth_pins); i++)
 	{
 		uint8_t pin = eth_pins[i];
-		gpio_set_mode(pin, GPIO_AF_INPUT_PU);
+		gpio_set_mode(pin, GPIO_AF_OUTPUT_PP);
 		gpio_set_af_mode(pin, GPIO_AFMODE_ETH);
 	}
 }
